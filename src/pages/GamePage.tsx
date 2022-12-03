ec840001e4a1a83fe793e22a3960251cf67d677b
Eren Yılmaz <eeyilmaz34@gmail.com>
2022-12-03 20:28:54 +0300

    Sayfa componentlere ayrıldı

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,29 +1,19 @@
-import SubscriberCard from "@components/subscriber/SubscriberCard";
-import CardContainer from "@modules/cardContainer/CardContainer";
-import { setCardStatus } from "@redux/reducers/cardPoint";
-import { RootState } from "@redux/store";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+
+import GamePageMiddle from "@modules/gamePageMiddle/GamePageMiddle";
+import GamePageHeader from  "@modules/gamaPageHeader/GamePageHeader";
+import GamePageFooter from "@modules/gamePageFooter/GamePageFooter";
 
 const GamePage = () => {
 
-    const cardStatus = useSelector((state: RootState) => state.cardStatus.status);
-    const cardPoint = useSelector((state: RootState) => state.cardPoint.point);
-
-    const dispatch = useDispatch();
-
-    const showCardNumber = () => {
-        if (cardPoint) {
-            dispatch(setCardStatus(true));
-        }
-    }
+   
 
     return (
         <>
-            <SubscriberCard userName='Semih' />
-            <CardContainer />
+            <GamePageHeader />
+                
+            <GamePageMiddle />
 
-            <button onClick={showCardNumber}>Click me</button>
+            <GamePageFooter />
         </>
     )
 }
