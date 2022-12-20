4bc4900bfddc76770678208fc3b894a0c1b7f7b7
Eren Yılmaz <eeyilmaz34@gmail.com>
2022-12-21 00:53:53 +0300

    Counter and Reveal Button controller added

--- src/components/buttons/reveal/RevealButton.tsx
+++ src/components/buttons/reveal/RevealButton.tsx
@@ -8,11 +8,15 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import {useState} from 'react';
 
+interface CounterProps {
+    status: () => void
+}
 export const CounterDiv = () => {
     const [counter, setCounter] = React.useState(3);
     React.useEffect(() => {
         counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
     },[counter])
+
     return (
         <div className={styles.counterDivs}>
             { counter }
@@ -28,7 +32,19 @@ const RevealButton = () => {
     const dispatch = useDispatch();
 
     
+
+    const [revealStatus, setRevealStatus] = useState(true);
+
+    const [counterStatus, setCounterStatus] = useState(false);
+
+    /*const [startStatus, setStartStatus] = useState(false);
+
+    const countStartStatus = (status:boolean) => {
+        setStartStatus(status)
+    }*/
+
     const showCardNumber = () => {
+        setCounterStatus(true)
         if (cardPoint) {
             dispatch(setCardStatus(true));
         }
@@ -38,9 +54,12 @@ const RevealButton = () => {
 
     return (
         <>
-            <button onClick={showCardNumber} className={styles.revealButton}>Reveal Cards</button>
-
-            <StartNewButton />
+            <button onClick={showCardNumber} className={`${styles.button} ${revealStatus === false ? styles.displayNoneButton : styles.revealButton }`}>Reveal Cards {startStatus}</button>
+            { counterStatus
+                ? <CounterDiv />
+                : <></>
+            }
+            <StartNewButton revealCard = { revealStatus }/>
         </>
     )
 }
