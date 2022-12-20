5a5d17bbbf36504d8f56de78c297195af2b92566
Eren Yılmaz <eeyilmaz34@gmail.com>
2022-12-21 00:52:05 +0300

    status controller added

--- src/components/buttons/startNew/StartNewButton.tsx
+++ src/components/buttons/startNew/StartNewButton.tsx
@@ -1,12 +1,12 @@
 import styles from "./StartNewButton.module.css"
 
-const StartNewButton = () => {
+const StartNewButton = (props: { revealCard: boolean}) => {
 
     const startNewVote = () => {
 
     }
     return (
-        <button onClick={startNewVote} className={styles.startButton}>Start New Vote</button>
+        <button onClick={startNewVote} className={`${styles.button} ${props.revealCard === false ? styles.displayNoneButton : styles.startButton }`}>Start New Vote</button>
     )
 }
 
