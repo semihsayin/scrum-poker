1d146ec5c51b58b0e69e527e953410ca0b7b2587
eerenyilmaz <eeyilmaz34@gmail.com>
2022-12-29 16:56:03 +0300

    Table componenti içerisinde geçiş sıralamaları ayarlandı

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,12 +1,27 @@
 import styles from './Table.module.css';
 import RevealButton from '@components/buttons/reveal/RevealButton';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '@redux/store';
 
 const Table = () => {
+    const cardPoint = useSelector((state: RootState) => state.cardPoint.point);
+    const [status, setStatus] = useState(false);
+
+    
+    const changeStatus = (statusC:boolean) => {
+        setStatus(statusC);
+    }
+
+    
     return (
         <div className={styles.tableDiv}>
-            Pick your card!
+            { cardPoint == null
+                ? <span>Pick your card! { cardPoint }</span>
+                : <RevealButton changeStatus= { changeStatus }/>
+            }
 
-            <RevealButton />
+            
         </div>
     )
 }
