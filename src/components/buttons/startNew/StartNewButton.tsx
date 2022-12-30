297b270664dbae99b42af94adf946915706800ac
eerenyilmaz <eeyilmaz34@gmail.com>
2022-12-30 17:48:56 +0300

    Card bağlantı ayarları yapıldı

--- src/components/buttons/startNew/StartNewButton.tsx
+++ src/components/buttons/startNew/StartNewButton.tsx
@@ -1,12 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./StartNewButton.module.css"
+import { setCardPoint } from '@redux/reducers/cardPoint';
+import { RootState } from "@redux/store";
 
 const StartNewButton = (props: { revealCard: boolean}) => {
-
+    const dispatch = useDispatch();
+    const cardPoint = useSelector((state: RootState) => state.cardPoint.point);
+    
     const startNewVote = () => {
-
+        dispatch(setCardPoint(null));
     }
     return (
-        <button onClick={startNewVote} className={`${styles.button} ${props.revealCard === true ? styles.displayNoneButton : styles.startButton }`}>Start New Vote</button>
+        <div className={`${props.revealCard === true ? styles.displayNoneButton : '' }`}>
+            <button onClick={startNewVote} className={`${styles.button} ${props.revealCard === true ? styles.displayNoneButton : styles.startButton }`}>Start New Vote </button>
+        </div>
     )
 }
 
