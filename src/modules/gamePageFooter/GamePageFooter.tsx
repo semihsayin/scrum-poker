8fcee6d542b27e86e867344480709af09748c1f7
eerenyilmaz <eeyilmaz34@gmail.com>
2023-01-02 14:41:03 +0300

    CardContainer koşullu bir şekilde gösterilcek şekilde değiştirildi

--- src/modules/gamePageFooter/GamePageFooter.tsx
+++ src/modules/gamePageFooter/GamePageFooter.tsx
@@ -1,10 +1,24 @@
+import { RootState } from '@redux/store';
 import CardContainer from "@modules/cardContainer/CardContainer";
+import { useSelector } from "react-redux";
 import styles from "./GamePageFooter.module.css"
 const GamePageFooter = () => {
+    const cardStatus = useSelector((state: RootState) => state.cardStatus.status);
+    const counterStatus = useSelector((state: RootState) => state.cardStatus.status);
+
+    console.log(counterStatus)
     return (
         <div className={styles.container}>
-            <div style={{ width: '100%',fontSize: '1.8rem',textAlign:'center'}}>Choose your card</div>
-            <CardContainer />
+            <div style={{ width: '100%',fontSize: '1.8rem',textAlign:'center'}}>
+                { cardStatus
+                    ? <span>Counting votes...</span>
+                    : <span>Choose your card</span>
+                }
+            </div>
+            { !counterStatus
+                ? <CardContainer />
+                : <></>
+            }
         </div>
     )
 }
