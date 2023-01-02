0a12a345f94d329edfc9b9c7336845e236938dae
eerenyilmaz <eeyilmaz34@gmail.com>
2023-01-02 16:25:08 +0300

    Footer kısmında duruma göre gösterilme ayarlandı

--- src/modules/gamePageFooter/GamePageFooter.tsx
+++ src/modules/gamePageFooter/GamePageFooter.tsx
@@ -5,20 +5,22 @@ import styles from "./GamePageFooter.module.css"
 const GamePageFooter = () => {
     const cardStatus = useSelector((state: RootState) => state.cardStatus.status);
     const counterStatus = useSelector((state: RootState) => state.cardStatus.status);
-
-    console.log(counterStatus)
+    const avarageStatus = useSelector((state: RootState) => state.avarageStatus.status);
     return (
         <div className={styles.container}>
             <div style={{ width: '100%',fontSize: '1.8rem',textAlign:'center'}}>
-                { cardStatus
-                    ? <span>Counting votes...</span>
-                    : <span>Choose your card</span>
+                { !counterStatus
+                    ? <span>Choose your card</span>
+                    : avarageStatus 
+                        ?<span>Done</span>
+                        :<span>Counting votes...</span>
                 }
             </div>
-            { !counterStatus
-                ? <CardContainer />
-                : <></>
-            }
+          
+          { avarageStatus
+                ? <></>
+                : <CardContainer />
+          }
         </div>
     )
 }
