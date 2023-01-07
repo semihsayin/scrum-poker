0a24a2ff60a7ec552e78bf229b63416a261156e2
Eren Yılmaz <eeyilmaz34@gmail.com>
2023-01-07 21:09:09 +0300

    Diğer kullanıcı kartları eklendi

--- src/modules/gamaPageHeader/GamePageHeader.tsx
+++ src/modules/gamaPageHeader/GamePageHeader.tsx
@@ -1,9 +1,24 @@
+import { RootState } from "@redux/store";
+import { useSelector } from "react-redux";
 import styles from "./GamePageHeader.module.css"
 
 const GamePageHeader = () => {
+    const cardPoint = useSelector((state: RootState) => state.cardPoint.point);
+    const cardStatus = useSelector((state: RootState) => state.cardStatus.status);
+    const counterStatus = useSelector((state: RootState) => state.counterStatus.status);
+
+    const numbers = [8,13,21,34,55,89];
     return (
         <div className={styles.container}>
-            
+            <ul className={styles.ulList}>
+                {numbers.map(number => (
+                    <li>
+                        <div className={`${styles.ulList} ${cardPoint == null ? '' : (counterStatus == true ? styles.card : styles.selected) }`}>
+                            { number }
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
