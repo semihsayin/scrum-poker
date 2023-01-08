a15ad6e1f523b30f54342da5fd08c1cd1f357900
Eren Yılmaz <eeyilmaz34@gmail.com>
2023-01-08 17:31:00 +0300

    Diğer oyuncuların oylarının toplamı ve averaj hesabı için ayarlar yapıldı

--- src/modules/gamaPageHeader/GamePageHeader.tsx
+++ src/modules/gamaPageHeader/GamePageHeader.tsx
@@ -1,4 +1,6 @@
+import { setPlayerNumber, setSumOfVote } from "@redux/reducers/cardPoint";
 import { RootState } from "@redux/store";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import styles from "./GamePageHeader.module.css"
 
@@ -6,18 +8,37 @@ const GamePageHeader = () => {
     const cardPoint = useSelector((state: RootState) => state.cardPoint.point);
     const cardStatus = useSelector((state: RootState) => state.cardStatus.status);
     const counterStatus = useSelector((state: RootState) => state.counterStatus.status);
+    const playerNumber = useSelector((state: RootState) => state.playerNumber.playerNumber);
+    const sumOfVote = useSelector((state: RootState) => state.sumOfVoter.sumOfVote);
 
     const numbers = [8,13,21,34,55,89];
+    let sum = 0;
+
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+        
+    }
+    
+    const dispatch = useDispatch();
+    dispatch(setPlayerNumber(numbers.length))
+    dispatch(setSumOfVote(sum))
+
+
     return (
         <div className={styles.container}>
             <ul className={styles.ulList}>
                 {numbers.map(number => (
-                    <li>
-                        <div className={`${styles.ulList} ${cardPoint == null ? '' : (counterStatus == true ? styles.card : styles.selected) }`}>
-                            { number }
+                    <li key={number}>
+                        <div style={{display:'flex', alignItems: 'center', justifyContent: 'center'}} className={`${styles.subsCard} ${cardPoint == null ? '' : (counterStatus == true ? styles.cards : styles.selected) }`}>
+                            
+                            { (cardPoint != null && cardStatus && counterStatus)
+                                ? <span>{ number }</span>
+                                :<></>
+                            }
                         </div>
                     </li>
                 ))}
+                
             </ul>
         </div>
     )
