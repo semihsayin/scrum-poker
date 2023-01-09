66693b0c8bc15cf781bd7f870684869e3e5baa74
eerenyilmaz <eeyilmaz34@gmail.com>
2023-01-09 17:44:30 +0300

    Fixed

--- src/components/averageCards/AverageCards.tsx
+++ src/components/averageCards/AverageCards.tsx
@@ -3,8 +3,19 @@ import styles from './AverageCards.module.css'
 const AverageCards = () => {
 
     return (
-        <>
-        </>
+        <div className={`${styles.halfDiv} ${styles.vote}`}>
+            <div style={{ display:'flex', justifyContent:'center',marginBottom: '10px'}}>
+                <div className={styles.stick}></div>
+            </div>
+
+            <div style={{ display:'flex', justifyContent:'center',marginBottom: '10px'}}>
+                <div className={styles.cardAvarage}></div>
+            </div>
+
+            <div style={{ display:'flex', justifyContent:'center',marginBottom: '10px'}}>
+                <span style={{fontSize: '1.2rem'}}>1 Vote</span>
+            </div>
+        </div>
     )
 }
 
