83ba7b2dabbe6ab924705239f5e6125f9b48c096
eerenyilmaz <eeyilmaz34@gmail.com>
2023-01-10 11:30:45 +0300

    5 sayısı için eksik olan düzenlemeler yapıldı

--- src/modules/avarage/Avarage.tsx
+++ src/modules/avarage/Avarage.tsx
@@ -9,12 +9,13 @@ const Avarage = () => {
     const playerNumber = useSelector((state: RootState) => state.playerNumber.playerNumber);
     const sumOfVote = useSelector((state: RootState) => state.sumOfVoter.sumOfVote);
     const votePoint = useSelector((state:RootState) => state.numberOfVotes)
+    const array = useSelector((state: RootState) => state.numbersArray);
 
     let average;
     if(typeof cardPoint === 'number') {
         average = ((cardPoint + sumOfVote) / playerNumber + 1).toFixed(1)
     }
-    
+    console.log()
     return (
         <div style={{width: '100%',height: '70%',display: 'flex', justifyContent:'center',alignItems:'center'}}>
             
